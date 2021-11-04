89c4be29ecbd59876995ba3a8be207b386303e96
Daniel Conger <dan@remihq.com>
2021-11-04 13:43:04 -0600

    initial

---
+++ src/main.ts
@@ -0,0 +1,70 @@
+import * as github from '@actions/github';
+import * as core from '@actions/core';
+
+interface PullRequest {
+  title: string;
+  body: string;
+  branch: string;
+  number: number;
+  labels: string[] | null;
+  assignees: string[] | null;
+}
+
+async function run(): Promise<void> {
+  try {
+    const pull = await getMergedPullRequest(
+      core.getInput('github_token'),
+      github.context.repo.owner,
+      github.context.repo.repo,
+      github.context.sha
+    );
+    if (!pull) {
+      core.debug('pull request not found');
+      return;
+    }
+
+    core.setOutput('title', pull.title);
+    core.setOutput('branch', pull.branch);
+    core.setOutput('body', pull.body);
+    core.setOutput('number', pull.number);
+    core.setOutput('labels', pull.labels?.join('\n'));
+    core.setOutput('assignees', pull.assignees?.join('\n'));
+  } catch (e) {
+    core.error(e);
+    core.setFailed(e.message);
+  }
+}
+
+async function getMergedPullRequest(
+  githubToken: string,
+  owner: string,
+  repo: string,
+  sha: string
+): Promise<PullRequest | null> {
+  const client = new github.GitHub(githubToken);
+
+  const resp = await client.pulls.list({
+    owner,
+    repo,
+    sort: 'updated',
+    direction: 'desc',
+    state: 'closed',
+    per_page: 100
+  });
+
+  const pull = resp.data.find(p => p.merge_commit_sha === sha);
+  if (!pull) {
+    return null;
+  }
+
+  return {
+    branch: pull.head.ref,
+    title: pull.title,
+    body: pull.body,
+    number: pull.number,
+    labels: pull.labels.map(l => l.name),
+    assignees: pull.assignees.map(a => a.login)
+  };
+}
+
+run();
